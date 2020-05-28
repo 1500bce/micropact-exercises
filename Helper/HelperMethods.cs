b9f510a13d77812745c763551d7e0282527bbd72
tntmzn <tintintiamzon@live.com>
2020-05-28 12:22:48 +0800

    separated helper methods

--- Helper/HelperMethods.cs
+++ Helper/HelperMethods.cs
@@ -2,7 +2,11 @@
 
 namespace Helper
 {
-    public class HelperMethods
+    public static class HelperMethods
     {
+        public static int ToInt(this string input)
+        {
+            return Convert.ToInt32(input);
+        }
     }
 }
--- Remove Character From String/Program.cs
+++ Remove Character From String/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Net.Http.Headers;
+using Helper;
 
 namespace Remove_Character_From_String
 {
